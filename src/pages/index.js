b0bd7e5b7af732d2b6cf50034781106b61f2f204
Jonas Reitmann <jonas@closelink.net>
2019-10-18 12:21:41 +0200

    Add repro example

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
 import React from "react"
+import PropTypes from 'prop-types';
 
-export default () => <div>Hello world!</div>
+const _MyComponent = ({ children, name }) => (
+  <div>{children} {name}</div>
+)
+
+_MyComponent.propTypes = {
+  children: PropTypes.node
+}
+
+export default () => <_MyComponent>Hello World</_MyComponent>
